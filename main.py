cc88381b243c371cb04eead733428b0d6b0ecd7f
servgo <sergioveguegonzalez@gmail.com>
2023-05-25 18:26:01 +0200

    ganancias

--- main.py
+++ main.py
@@ -9,31 +9,37 @@ data = {
     'EsSuave': [0, 0, 1, 1, 0, 1, 1, 0],
     'EsVenenosa': [0, 0, 0, 1, 1, 1, 1, 1]
 }
+
 df = pd.DataFrame(data)
+
 def entropia(columna):
     valores = columna.value_counts()
     num = len(columna)
-    entropia = 0
+    entr = 0
     for count in valores:
         p = count / num
-        entropia -= p * math.log2(p)
-    return entropia
+        entr -= p * math.log2(p)
+    return entr
+
+def ganancia(column, target_column):
+    unique_values = column.unique()
+    total_entropy = entropia(target_column)
+    weighted_entropy = 0
+    for value in unique_values:
+        subset = target_column[column == value]
+        subset_entropy = entropia(subset)
+        weighted_entropy += (len(subset) / len(column)) * subset_entropy
+    gain = total_entropy - weighted_entropy
+    return gain
 
 entropiaVenenosa = entropia(df['EsVenenosa'])
-entropiaPesaMucho = entropia(df['PesaMucho'])
-entropiaEsMaloliente = entropia(df['EsMaloliente'])
-entropiaManchas = entropia(df['EsConManchas'])
-entropiaSuave = entropia(df['EsSuave'])
+entropiaPesaMucho = ganancia(df['PesaMucho'], df['EsVenenosa'])
+entropiaEsMaloliente = ganancia(df['EsMaloliente'], df['EsVenenosa'])
+entropiaManchas = ganancia(df['EsConManchas'], df['EsVenenosa'])
+entropiaSuave = ganancia(df['EsSuave'], df['EsVenenosa'])
+
 print("Entropía de EsVenenosa:", entropiaVenenosa)
-print("Entropía de PesaMucho:", entropiaPesaMucho)
-print("Entropía de EsMaloliente:", entropiaEsMaloliente)
-print("Entropía de EsConManchas:", entropiaManchas)
-print("Entropía de EsSuave:", entropiaSuave)
-gananciaPesa = entropiaVenenosa - entropiaPesaMucho
-gananciaMaloliente = entropiaVenenosa - entropiaEsMaloliente
-gananciaManchas = entropiaVenenosa - entropiaManchas
-gananciaSuave = entropiaVenenosa - entropiaSuave
-print("Ganancia de PesaMucho:", gananciaPesa)
-print("Ganancaia de EsMaloliente:", gananciaMaloliente)
-print("Ganancia de EsConManchas:", gananciaManchas)
-print("Ganancia de EsSuave:", gananciaSuave)
+print("Ganancia de PesaMucho:", entropiaPesaMucho)
+print("Ganancia de EsMaloliente:", entropiaEsMaloliente)
+print("Ganancia de EsConManchas:", entropiaManchas)
+print("Ganancia de EsSuave:", entropiaSuave)
