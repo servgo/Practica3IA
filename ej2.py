00fccb8d4ebdc3ac046603883bb770a1ca3603ef
servgo <sergioveguegonzalez@gmail.com>
2023-05-26 19:11:02 +0200

    ej2 a

--- ej2.py
+++ ej2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Datos de entrada y salida objetivo
-X = np.array([
+x = np.array([
     [1, 0, 1, 0, 0, 0],
     [1, 0, 1, 1, 0, 0],
     [1, 0, 1, 0, 1, 0],
@@ -18,37 +18,39 @@ X = np.array([
     [0, 1, 1, 1, 1, 0]
 ])
 
-X
 
-t = np.array([1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0])
+objetivo = np.array([1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0])
 
 # Inicialización de pesos y umbral
 np.random.seed()
-weights = np.random.rand(X.shape[1])
-threshold = np.random.rand()
+pesos = [0.1] * 6
+umbral = 0.5
 
 # Parámetros de aprendizaje
-learning_rate = 0.1
-epochs = 10
+tasa_aprendizaje = 0.1
+epocas = 5
 
 # Entrenamiento del perceptrón
-for epoch in range(epochs):
-    print("Época:", epoch+1)
-    print("Pesos:", weights)
-    for i in range(X.shape[0]):
+for epoca in range(epocas):
+    print("Época:", epoca+1)
+    print("Pesos:", pesos)
+    for i in range(x.shape[0]):
         # Cálculo de la salida del perceptrón multiplicando las matrices
-        net = np.dot(X[i], weights)
+        net = np.dot(x[i], pesos)
         # Si la preducción es mayor o igual que el umbral lo establecemos a 1
-        y_pred = 1 if net >= threshold else 0
+        y_pred = 1 if net >= umbral else 0
 
         # Actualización de pesos y umbral comprobando si coincide con lo esperado
-        if y_pred != t[i]:
-            weights += learning_rate * (t[i] - y_pred) * X[i]
-            threshold += learning_rate * (t[i] - y_pred)
+        if y_pred != objetivo[i]:
+            pesos += tasa_aprendizaje * (objetivo[i] - y_pred) * x[i]
+            umbral += tasa_aprendizaje * (objetivo[i] - y_pred)
 
         print("Salida:", y_pred)
 
     print("--------------------")
 
-print("Pesos finales:", weights)
-print("Umbral final:", threshold)
+print("Pesos finales:", pesos)
+print("Umbral final:", umbral)
+print("Como podemos apreciar, a partir de la época 3 el perceptrón ya ha aprendido y nos da la salida esperada")
+
+
