d4ef989f9621073221c412d895129d8af85334a5
servgo <sergioveguegonzalez@gmail.com>
2023-05-26 18:57:00 +0200

    ej2 a

--- ej2.py
+++ ej2.py
@@ -2,13 +2,23 @@ import numpy as np
 
 # Datos de entrada y salida objetivo
 X = np.array([
-    [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1],
-    [1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1],
-    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1],
-    [0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0],
-    [0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0]
+    [1, 0, 1, 0, 0, 0],
+    [1, 0, 1, 1, 0, 0],
+    [1, 0, 1, 0, 1, 0],
+    [1, 1, 0, 0, 1, 1],
+    [1, 1, 1, 1, 0, 0],
+    [1, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0, 1],
+    [0, 1, 1, 0, 1, 1],
+    [0, 0, 0, 1, 1, 0],
+    [0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 1],
+    [0, 1, 1, 0, 0, 1],
+    [0, 1, 1, 1, 1, 0]
 ])
+
+X
 
 t = np.array([1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0])
 
@@ -23,10 +33,12 @@ epochs = 10
 
 # Entrenamiento del perceptrón
 for epoch in range(epochs):
+    print("Época:", epoch+1)
+    print("Pesos:", weights)
     for i in range(X.shape[0]):
         # Cálculo de la salida del perceptrón multiplicando las matrices
         net = np.dot(X[i], weights)
-        # Si la preducción es  es mayor o igual que el umbral lo establecemos a 1
+        # Si la preducción es mayor o igual que el umbral lo establecemos a 1
         y_pred = 1 if net >= threshold else 0
 
         # Actualización de pesos y umbral comprobando si coincide con lo esperado
@@ -34,6 +46,9 @@ for epoch in range(epochs):
             weights += learning_rate * (t[i] - y_pred) * X[i]
             threshold += learning_rate * (t[i] - y_pred)
 
+        print("Salida:", y_pred)
+
+    print("--------------------")
 
 print("Pesos finales:", weights)
 print("Umbral final:", threshold)
